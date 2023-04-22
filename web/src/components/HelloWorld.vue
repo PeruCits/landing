ef00de427df40014ab12416c71cf372186dc5951
jaime garrido <jaime.garrido@bbva.com>
2023-04-22 11:20:23 -0500

    initial commit

---
+++ web/src/components/HelloWorld.vue
@@ -0,0 +1,228 @@
+<template>
+<h1>Modelo de buenas practicas de desarrollo</h1>
+<br/>
+
+  <table>
+    <tr>
+      <th>BackEnd</th>
+      <th>FrontEnd</th>
+      <th>Testing</th>
+      <th>Seguridad</th>
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+
+    </tr>
+    <tr>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+      <td>
+        <video width="320" height="240" controls>
+          <source src="https://www.youtube.com/watch?v=_bzacHQlLlI" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.fila {
+  background-color: #42b983;
+  justify-content: space-between;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+th {
+  width: 400px;
+}
+</style>
